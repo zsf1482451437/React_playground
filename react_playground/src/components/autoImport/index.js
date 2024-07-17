b51e9a872a0ab18e521cc5cadf590112d7cd17d3
ZSF <zhaisifeng@taotens.com>
2024-07-17 17:07:26 +0800

    feat: cart component

--- react_playground/src/components/autoImport/index.js
+++ react_playground/src/components/autoImport/index.js
@@ -2,3 +2,4 @@ export { default as Website } from 'pages/website';
 export { default as Header } from 'components/base/Header';
 // export { default as Star } from 'pages/animation/star';
 // export { default as Fire } from 'pages/animation/fire';
+export { default as Card } from 'components/base/Card';
---
+++ react_playground/src/components/base/Card/index.jsx
@@ -0,0 +1,45 @@
+import classNames from 'classnames/bind';
+import styles from './index.module.scss';
+
+const cn = classNames.bind(styles);
+
+// 定义课程信息数组
+const courses = [
+  {
+    title: '网络请求',
+    date: '04.11.2022',
+  },
+  {
+    title: '状态管理',
+    date: '04.11.2022',
+  },
+  {
+    title: '第三方组件库',
+    date: '04.11.2022',
+  },
+  // 添加更多课程...
+];
+
+const Card = () => {
+  return (
+    <div className={cn('ag-format-container')}>
+      <div className={cn('ag-courses_box')}>
+        {courses.map((course, index) => (
+          <div key={index} className={cn('ag-courses_item')}>
+            <a href="#" className={cn('ag-courses-item_link')}>
+              <div className={cn('ag-courses-item_bg')}></div>
+              <div className={cn('ag-courses-item_title')}>{course.title}</div>
+              <div className={cn('ag-courses-item_date-box')}>
+                <span className={cn('ag-courses-item_date')}>
+                  {course.date}
+                </span>
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
