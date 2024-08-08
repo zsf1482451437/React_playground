f46d33d75f393a680a9ff6b3ada4e2cdf70f64fd
ZSF <1482451437@qq.com>
2024-08-08 14:39:53 +0800

    fix: proxy

--- react_playground/api/proxy.js
+++
@@ -1,9 +0,0 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
-export default createProxyMiddleware({
-  target: 'https://api.cntv.cn',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '', // 去掉路径中的 /api
-  },
-});
