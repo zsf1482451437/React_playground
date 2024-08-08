168c2cdfee04d2241cd1215486fd548ce9f49a55
ZSF <1482451437@qq.com>
2024-08-08 15:03:33 +0800

    feat: corner

--- react_playground/src/components/autoImport/index.js
+++ react_playground/src/components/autoImport/index.js
@@ -4,4 +4,5 @@ export { default as Olympic } from 'pages/olympics';
 // 组件
 export { default as Header } from 'components/base/Header';
 export { default as Card } from 'components/base/Card';
+export { default as Corner } from 'components/base/Corner';
 // export { default as Loading } from 'components/base/Loading';
