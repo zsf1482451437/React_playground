54f98006ed32a1fd3f2c62b6a35d41a8b2795acc
ZSF <1482451437@qq.com>
2024-08-09 15:25:50 +0800

    style: theme

--- react_playground/tailwind.config.js
+++ react_playground/tailwind.config.js
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+      },
+      transitionTimingFunction: {
+        'out-back': 'cubic-bezier(0.34, 1.56, 0.64, 1)',
+      },
+      transformOrigin: {
+        'center-bottom': '50% 100%',
+      },
+      keyframes: {
+        // spinner
+        'intense-pulse': {
+          '0%': { opacity: 0.2 },
+          '20%': { opacity: 1, transform: 'translateX(1px)' },
+          '100%': { opacity: 0.2 },
+        },
+        // 404 page
+        floating: {
+          '50%': { transform: 'translate(0, -10%)' },
+        },
+        // hit counter
+        'backdrop-flicker': {
+          '50%': { 'backdrop-filter': 'blur(1px) brightness(20%)' },
+        },
+        // post outdated alert
+        'move-bg-y': {
+          '0%': { 'background-position': '0 0' },
+          '100%': { 'background-position': '0 100%' },
+        },
+      },
+    },
   },
   plugins: [],
 };
