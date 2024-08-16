6ad2dfbe33b6689a4d671dc3daf053fbcd6f9d81
ZSF <1482451437@qq.com>
2024-08-16 15:59:00 +0800

    style: card

--- react_playground/src/components/base/Card/index.jsx
+++ react_playground/src/components/base/Card/index.jsx
@@ -9,28 +9,20 @@ const courses = [
     title: '网络请求',
     date: '04.11.2022',
   },
-  {
-    title: '状态管理',
-    date: '04.11.2022',
-  },
-  {
-    title: '第三方组件库',
-    date: '04.11.2022',
-  },
   // 添加更多课程...
 ];
 
 const Card = () => {
   return (
-    <div className={cn('ag-format-container')}>
-      <div className={cn('ag-courses_box')}>
+    <div className={cn('ag_format_container')}>
+      <div className={cn('ag_courses_box')}>
         {courses.map((course, index) => (
-          <div key={index} className={cn('ag-courses_item')}>
-            <a href="/" className={cn('ag-courses-item_link')}>
-              <div className={cn('ag-courses-item_bg')}></div>
-              <div className={cn('ag-courses-item_title')}>{course.title}</div>
-              <div className={cn('ag-courses-item_date-box')}>
-                <span className={cn('ag-courses-item_date')}>
+          <div key={index} className={cn('ag_courses_item')}>
+            <a href="/" className={cn('ag_courses_item_link')}>
+              <div className={cn('ag_courses_item_bg')}></div>
+              <div className={cn('ag_courses_item_title')}>{course.title}</div>
+              <div className={cn('ag_courses_item_date_box')}>
+                <span className={cn('ag_courses_item_date')}>
                   {course.date}
                 </span>
               </div>
