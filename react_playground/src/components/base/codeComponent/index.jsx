5db86777688b3dc0bce57706c7fc2a0884091d3d
ZSF <1482451437@qq.com>
2024-08-16 16:01:02 +0800

    feat: code component

---
+++ react_playground/src/components/base/codeComponent/index.jsx
@@ -0,0 +1,27 @@
+import { CopyBlock, atomOneDark } from 'react-code-blocks';
+
+import classNames from 'classnames/bind';
+import styles from './index.module.scss';
+
+const cn = classNames.bind(styles);
+
+const CodeComponent = ({
+  showLineNumbers = true,
+  code = 'HELLO WORLD',
+  theme = atomOneDark,
+  language = 'jsx',
+}) => (
+  <div className={cn('code_container')}>
+    <CopyBlock
+      text={code}
+      showLineNumbers={showLineNumbers}
+      theme={theme}
+      language={language}
+      codeBlock={true}
+    />
+  </div>
+);
+
+CodeComponent.type = 'component';
+
+export default CodeComponent;
--- react_playground/src/pages/other/index.jsx
+++
@@ -1,8 +0,0 @@
-import { CodeBlock, nord } from 'react-code-blocks';
-const MyCodeComponent = () => (
-  <CodeBlock text="HELLO WORLD" theme={nord} language="text" />
-);
-
-MyCodeComponent.type = 'other';
-
-export default MyCodeComponent;
