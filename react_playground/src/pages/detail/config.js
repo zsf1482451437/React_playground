c0ab3ab057104280c8ce68d74123dc8a033079ce
ZSF <1482451437@qq.com>
2024-08-22 11:28:39 +0800

    feat: add tab to detail page

--- react_playground/src/pages/detail/config.js
+++ react_playground/src/pages/detail/config.js
@@ -10,13 +10,67 @@ import Particle from 'pages/animation/particle';
 
 import DETAIL from 'constants/detail';
 
+// 加载组件的源代码
+import CardSource from '!!raw-loader!components/base/Card/index.jsx';
+import GithubCornerSource from '!!raw-loader!components/base/GithubCorner/index.jsx';
+import HeaderSource from '!!raw-loader!components/base/Header/index.jsx';
+import LoadingSource from '!!raw-loader!components/base/Loading/index.jsx';
+import ReactLogoSource from '!!raw-loader!components/base/ReactLogo/index.jsx';
+import FlameSource from '!!raw-loader!pages/animation/flame/index.jsx';
+import ParticleSource from '!!raw-loader!pages/animation/particle/index.jsx';
+
+// 加载组件的样式
+import CardStyle from '!!raw-loader!components/base/Card/index.module.scss';
+import GithubCornerStyle from '!!raw-loader!components/base/GithubCorner/index.module.scss';
+import HeaderStyle from '!!raw-loader!components/base/Header/index.module.scss';
+import LoadingStyle from '!!raw-loader!components/base/Loading/index.module.scss';
+import ReactLogoStyle from '!!raw-loader!components/base/ReactLogo/index.module.scss';
+import FlameStyle from '!!raw-loader!pages/animation/flame/index.module.scss';
+import ParticleStyle from '!!raw-loader!pages/animation/particle/index.module.scss';
+
 const common_data = [
-  { path: DETAIL.CARD, component: <Card /> },
-  { path: DETAIL.GITHUB, component: <GithubCorner /> },
-  { path: DETAIL.HEADER, component: <Header /> },
-  { path: DETAIL.LOADING, component: <Loading /> },
-  { path: DETAIL.LOGO, component: <ReactLogo /> },
-  { path: DETAIL.FLAME, component: <Flame /> },
-  { path: DETAIL.PARTICLE, component: <Particle /> },
+  {
+    path: DETAIL.CARD,
+    component: <Card />,
+    code: CardSource,
+    style: CardStyle,
+  },
+  {
+    path: DETAIL.GITHUB,
+    component: <GithubCorner />,
+    code: GithubCornerSource,
+    style: GithubCornerStyle,
+  },
+  {
+    path: DETAIL.HEADER,
+    component: <Header />,
+    code: HeaderSource,
+    style: HeaderStyle,
+  },
+  {
+    path: DETAIL.LOADING,
+    component: <Loading />,
+    code: LoadingSource,
+    style: LoadingStyle,
+  },
+  {
+    path: DETAIL.LOGO,
+    component: <ReactLogo />,
+    code: ReactLogoSource,
+    style: ReactLogoStyle,
+  },
+  {
+    path: DETAIL.FLAME,
+    component: <Flame />,
+    code: FlameSource,
+    style: FlameStyle,
+  },
+  {
+    path: DETAIL.PARTICLE,
+    component: <Particle />,
+    code: ParticleSource,
+    style: ParticleStyle,
+  },
 ];
+
 export default common_data;
--- react_playground/src/pages/detail/index.jsx
+++ react_playground/src/pages/detail/index.jsx
@@ -3,19 +3,63 @@ import { useLocation } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import styles from './index.module.scss';
 
+import CodeComponent from 'components/base/codeComponent';
 import common_data from './config';
+import Tab from 'components/base/Tab';
 
 const cn = classNames.bind(styles);
 
+
+
 const Detail = () => {
   const location = useLocation();
+
   const [component, setComponent] = useState('暂无内容');
+  const [code, setCode] = useState('');
+  const [style, setStyle] = useState('');
+
   useEffect(() => {
     const path = location.pathname;
     const data = common_data.find((item) => item.path === path);
-    if (data?.component) setComponent(data.component);
+    if (data?.component) {
+      setComponent(data.component);
+      setCode(data.code); // 假设 common_data 中有 code 字段
+      setStyle(data.style);
+    }
   }, [location]);
-  return <div className={cn('container')}>{component}</div>;
+
+  const tabs = [
+    {
+      key: 'component',
+      label: 'Component',
+      content: <div className={cn('component_render')}>{component}</div>,
+    },
+    {
+      key: 'code',
+      label: 'Code',
+      content: code && (
+        <div className={cn('code_section')}>
+          <h2>index.jsx</h2>
+          <CodeComponent code={code} language="jsx" />
+        </div>
+      ),
+    },
+    {
+      key: 'style',
+      label: 'Style',
+      content: style && (
+        <div className={cn('code_section')}>
+          <h2>index.module.scss</h2>
+          <CodeComponent code={style} language="scss" />
+        </div>
+      ),
+    },
+  ];
+  return (
+    <div className={cn('container')}>
+      <Tab tabs={tabs} />
+    </div>
+  );
 };
 
 export default Detail;
