3e7ecd82ca3751037d9262ddf30d6e2f6ef2e61f
ZSF <1482451437@qq.com>
2024-08-22 11:27:41 +0800

    feat: tab

---
+++ react_playground/src/components/base/Tab/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import classNames from 'classnames/bind';
+import styles from './index.module.scss';
+
+const cn = classNames.bind(styles);
+
+const Tab = ({ tabs }) => {
+  const [activeTab, setActiveTab] = useState(tabs[0].key);
+
+  return (
+    <div className={cn('tab_container')}>
+      <div className={cn('tabs')}>
+        {tabs.map((tab) => (
+          <button
+            key={tab.key}
+            onClick={() => setActiveTab(tab.key)}
+            className={cn({ active: activeTab === tab.key })}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+      <div className={cn('tab_content')}>
+        {tabs.map((tab) => (
+          <div
+            key={tab.key}
+            className={cn('tab_panel', { active: activeTab === tab.key })}
+          >
+            {activeTab === tab.key && tab.content}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Tab;
